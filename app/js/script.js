bfa059340a53be3a7b06a6497115fd2b1110b146
Kari L <42503401+KJLarson@users.noreply.github.com>
2020-11-03 14:53:03 -0600

    Compile Handlebars template and add data to HTML

--- app/js/script.js
+++ app/js/script.js
@@ -1,10 +1,17 @@
+// import { compile } from 'handlebars';
+
 const dataURL = '/data/resume.json';
+const resumeBuilderContainer = document.querySelector('#resume-builder-container');
+const resumeTemplate = document.querySelector('#resume-template').innerHTML;
+
+const compiledResumeTemplate = Handlebars.compile(resumeTemplate);
 
 fetch(dataURL)
   .then(res => res.json())
   .then(data => {
     // code to handle response
-    console.log(data);
+    // console.log(data);
+    resumeBuilderContainer.innerHTML = compiledResumeTemplate(data);
   }).catch(err => {
     //code to handle errors
     console.error('Error: ', err);
